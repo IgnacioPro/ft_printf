0dd8dfcf44fee562bac9d689eaab309c20b5a553
Ignacio Horcada <IgnacioHB@MacBook-Pro-de-Ignacio.local>
2020-08-07 17:06:18 +0200

    streamlined

--- ft_display_pct.c
+++ ft_display_pct.c
@@ -6,7 +6,7 @@
 /*   By: IgnacioHB <IgnacioHB@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/07 16:17:32 by IgnacioHB         #+#    #+#             */
-/*   Updated: 2020/08/07 16:47:01 by IgnacioHB        ###   ########.fr       */
+/*   Updated: 2020/08/07 17:04:46 by IgnacioHB        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,20 @@ void ft_display_pct(printf_s *format, char c)
 {
     format->s_pct = format->width - 1;
     while (format->s_pct-- > 0 && format->tab != '-')
-        format->strlen += write(1, (format->zero_space != '0' ||
-            (format->dot == '.' && format->precision >= 0)) ? " " : "0", 1);
-    format->strlen += write(1, &c, 1);
+    {
+        if ((format->zero_space != '0') || 
+            (format->dot == '.' && format->precision >= 0))
+                write(1," ", 1);
+        else
+            write(1, "0", 1);
+        format->strlen++;
+    }
+    format->strlen ++;
+    write(1, &c, 1);
+
     while (format->s_pct-- >= 0)
-        format->strlen += write(1, " ", 1);
+    {
+        write(1, " ", 1);
+        format->strlen++;
+    }
 }
