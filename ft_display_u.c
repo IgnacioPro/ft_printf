012716d781e815c7a8ef1ff97ef5c8c948566c8b
Ignacio Horcada <IgnacioHB@MacBook-Pro-de-Ignacio.local>
2020-08-12 13:26:07 +0200

    streamlined

--- ft_display_u.c
+++ ft_display_u.c
@@ -6,7 +6,7 @@
 /*   By: IgnacioHB <IgnacioHB@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/05 12:52:52 by IgnacioHB         #+#    #+#             */
-/*   Updated: 2020/08/11 10:51:16 by IgnacioHB        ###   ########.fr       */
+/*   Updated: 2020/08/12 13:25:00 by IgnacioHB        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,18 @@ void    ft_display_u(printf_s *format, int nb)
 
 	unsigned_nb = nb;
 	len = ft_ulen(unsigned_nb);
-	space = format->width - ((format->precision <= len) ? len : format->precision);
+	if (format->precision <= len)
+		space = format->width - len;
+	if(format->precision > len)
+		space = format->width - format->precision;
 	zero = format->precision - len;
 	space += (nb == 0 && format->precision == 0 &&
 		format->dot == '.' && format->width > 0) ? 1 : 0;
+	ft_format_u(format, nb, len, space, zero);
+}
+
+void ft_format_u(printf_s *format, int nb, int len, int space, int zero)
+{
 	while (space-- > 0 && format->tab == ' ')
 		format->strlen += write(1, (format->zero_space != '0' ||
 			(format->dot == '.' && format->precision >= 0)) ? " " : "0", 1);
@@ -39,7 +47,6 @@ void    ft_display_u(printf_s *format, int nb)
 		format->strlen--;
 	format->strlen += len;
 }
-
 size_t ft_ulen(long nb)
 {
 	size_t len;
