8c54703dbf60420632103e09ac08239f31579b2d
Ignacio Horcada <IgnacioHB@MacBook-Pro-de-Ignacio.local>
2020-08-12 13:53:27 +0200

    streamline

--- ft_display_s.c
+++ ft_display_s.c
@@ -6,7 +6,7 @@
 /*   By: IgnacioHB <IgnacioHB@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/28 12:51:01 by IgnacioHB         #+#    #+#             */
-/*   Updated: 2020/08/11 18:25:23 by IgnacioHB        ###   ########.fr       */
+/*   Updated: 2020/08/12 13:52:24 by IgnacioHB        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,20 +19,26 @@ void ft_display_s(printf_s *format, char *str)
 
     if (str == NULL)
         str = "(null)";
-    len = ft_strlen(str);     
-    len -= (format->precision > 0 &&
-        format->precision < len ) ? (len - format->precision) : 0;
-    len -= (format->dot == '.' && format->tab == '-' &&
-        format->precision == 0 && format->width == 0)? len : 0;
-    len -= (format->dot == '.' && format->tab != '-' &&
-        format->precision == 0 && format->width == 0) ? len : 0;
-    len -= (format->precision == 0 && format->width > 0 &&
-        format->zero_space == ' ' && format->dot == '.') ? len : 0;
-    space = (format->width > 0) ? (format->width - len) : 0;
-    space += (format->tab == '-') ? 1 : 0;
+    len = ft_strlen(str);  
+       
+    if (format->precision > 0 && format->precision < len )
+        len -= len - format->precision;
+    if (format->precision == 0 && format->width > 0 &&
+        format->zero_space == ' ' && format->dot == '.')
+            len -= len;
+    if (format->dot == '.' && format-> width == 0 && format->precision == 0)
+        len -= len;
+    if (format->width > 0)
+        space = (format->width - len);
+    if (format->width <= 0)
+        space = 0;
+    if (format->tab == '-')
+        ++space;
+    ft_format_s(format, str, space, len);
+}
 
-    if (format->tab == '-' && format->zero_space == '0')
-        format->zero_space = ' ';
+void ft_format_s(printf_s *format, char *str, int space, int len)
+{
     while (space-- > 0 && format->tab != '-')
         format->strlen += write(1, &format->zero_space, 1);
     if (!(format->dot == '.' && format->precision == -1))
