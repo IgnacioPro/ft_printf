64d72adc2c80ac3bda4b5cc53b2a0ca9606e5481
Ignacio Horcada <IgnacioHB@MacBook-Pro-de-Ignacio.local>
2020-08-12 16:12:09 +0200

    norm

--- ft_display_u.c
+++ ft_display_u.c
@@ -6,7 +6,7 @@
 /*   By: IgnacioHB <IgnacioHB@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/05 12:52:52 by IgnacioHB         #+#    #+#             */
-/*   Updated: 2020/08/12 16:09:45 by IgnacioHB        ###   ########.fr       */
+/*   Updated: 2020/08/12 16:12:00 by IgnacioHB        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,7 @@ void	ft_display_u(printf_s *format, int nb)
 		format->strlen += write(1, "0", 1);
 	ft_format_u(format, nb, len, space);
 }
+
 void	ft_format_u(printf_s *format, int nb, int len, int space)
 {
 	if (!(nb == 0 && format->precision == 0 && format->dot == '.'))
@@ -63,7 +64,7 @@ size_t	ft_ulen(long nb)
 	}
 	while (nb > 0)
 	{
-		nb /= 10; 
+		nb /= 10;
 		len++;
 	}
 	return (len);
