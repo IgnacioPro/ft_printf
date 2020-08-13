298c3da60cd4627922469a05e56835809e665635
Ignacio Horcada <IgnacioHB@MacBook-Pro-de-Ignacio.local>
2020-08-13 13:10:53 +0200

    change struct norm

--- libftprintf.h
+++ libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: IgnacioHB <IgnacioHB@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/26 18:22:21 by IgnacioHB         #+#    #+#             */
-/*   Updated: 2020/08/13 13:08:46 by IgnacioHB        ###   ########.fr       */
+/*   Updated: 2020/08/13 13:10:47 by IgnacioHB        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,39 +31,39 @@ typedef	struct	s_printf
 	int		len_pct;
 	int		n_d;
 	va_list argptr;
-}		t_printf;
+}				t_printf;
 
-int		ft_printf(const char *s, ...);
-void	ft_picker(printf_s *format);
-void	ft_format_read(printf_s *format);
-void	ft_dot(printf_s *format);
-size_t	ft_intlen(long nb);
-void	ft_formatter(printf_s *format);
-void	ft_args(printf_s *format);
-void	ft_display_c(printf_s *format, int nb);
-void	ft_display_s(printf_s *format, char *str);
-void	ft_display_d(printf_s *format, int nb);
-void	ft_display_x(printf_s *format, unsigned long nb);
-void	ft_display_p(printf_s *format, char *str);
-size_t	ft_xlen(size_t nb);
-void	ft_to_hexa(printf_s *format, unsigned long number);
-void	ft_mem(printf_s *format, char *str);
-void	ft_display_u(printf_s *format, int nb);
-void	ft_putunbr(int n, int fd);
-void	ft_print_number(int n, int fd);
-void	ft_display_pct(printf_s *format, char c);
-void	ft_is_unsigned(unsigned n, int fd);
-void	ft_write_unsigned(int n, int fd);
-size_t	ft_ulen(long nb);
-void	ft_precision_width(printf_s *format, int nb, int zero);
-void	ft_space_blank(printf_s *format, int space, int len, int nb);
-void	ft_zeros_spaces(printf_s *format, int nb,
-			int space, int len, int zero);
-void	ft_format_p(printf_s *format, char *str,
-			int space, int plus);
-void	ft_format_u(printf_s *format, int nb, int len, int space);
-void	ft_format_s(printf_s *format, char *str, int space, int len);
-void	ft_format_x(printf_s *format, int space,
-		int zero, unsigned long nb);
+int				ft_printf(const char *s, ...);
+void			ft_picker(printf_s *format);
+void			ft_format_read(printf_s *format);
+void			ft_dot(printf_s *format);
+size_t			ft_intlen(long nb);
+void			ft_formatter(printf_s *format);
+void			ft_args(printf_s *format);
+void			ft_display_c(printf_s *format, int nb);
+void			ft_display_s(printf_s *format, char *str);
+void			ft_display_d(printf_s *format, int nb);
+void			ft_display_x(printf_s *format, unsigned long nb);
+void			ft_display_p(printf_s *format, char *str);
+size_t			ft_xlen(size_t nb);
+void			ft_to_hexa(printf_s *format, unsigned long number);
+void			ft_mem(printf_s *format, char *str);
+void			ft_display_u(printf_s *format, int nb);
+void			ft_putunbr(int n, int fd);
+void			ft_print_number(int n, int fd);
+void			ft_display_pct(printf_s *format, char c);
+void			ft_is_unsigned(unsigned n, int fd);
+void			ft_write_unsigned(int n, int fd);
+size_t			ft_ulen(long nb);
+void			ft_precision_width(printf_s *format, int nb, int zero);
+void			ft_space_blank(printf_s *format, int space, int len, int nb);
+void			ft_zeros_spaces(printf_s *format, int nb,
+					int space, int len, int zero);
+void			ft_format_p(printf_s *format, char *str,
+					int space, int plus);
+void			ft_format_u(printf_s *format, int nb, int len, int space);
+void			ft_format_s(printf_s *format, char *str, int space, int len);
+void			ft_format_x(printf_s *format, int space,
+					int zero, unsigned long nb);
 
 #endif
